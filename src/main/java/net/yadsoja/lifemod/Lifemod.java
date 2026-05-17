da2ec5bdc8e7ae392e39945f238030e1a0e72511
Yadsoja <expanby53@gmail.com>
2026-05-18 00:39:46 +0200

    supression d'un commentaire de test

--- src/main/java/net/yadsoja/lifemod/Lifemod.java
+++ src/main/java/net/yadsoja/lifemod/Lifemod.java
@@ -18,7 +18,6 @@ public class Lifemod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		//sdz
 
 		LOGGER.info("Hello Fabric world!");
 	}
