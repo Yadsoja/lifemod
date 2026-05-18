8eb36bb94bbb5c5796cf8c03788447fb8b77d2cf
Yadsoja <expanby53@gmail.com>
2026-05-18 10:07:42 +0200

    Début d'ajout des commandes de gestion des curses

---
+++ src/main/java/net/yadsoja/lifemod/command/CurseCommand.java
@@ -0,0 +1,153 @@
+package net.yadsoja.lifemod.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+
+import net.yadsoja.lifemod.curse.CurseManager;
+
+public class CurseCommand {
+
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+
+        dispatcher.register(
+                CommandManager.literal("curse")
+
+                        // /curse => info
+                        .executes(ctx -> {
+                            ctx.getSource().sendFeedback(
+                                    () -> Text.literal(
+                                            """
+                                            §6=== Curse System ===
+                                            /curse add
+                                            /curse add <name>
+                                            /curse remove
+                                            /curse remove <name>
+                                            /curse timer <minutes>
+                                            /curse toggle
+                                            """
+                                    ),
+                                    false
+                            );
+                            return 1;
+                        })
+
+                        // /curse add
+                        .then(CommandManager.literal("add")
+
+                                // /curse add => list curses
+                                .executes(ctx -> {
+                                    ctx.getSource().sendFeedback(
+                                            () -> Text.literal("Available curses: " + CurseManager.availableCurses),
+                                            false
+                                    );
+                                    return 1;
+                                })
+
+                                // /curse add freeze
+                                .then(CommandManager.argument("curse", StringArgumentType.word())
+                                        .executes(ctx -> {
+
+                                            String curse = StringArgumentType.getString(ctx, "curse");
+
+                                            if (!CurseManager.availableCurses.contains(curse)) {
+                                                ctx.getSource().sendFeedback(
+                                                        () -> Text.literal("Unknown curse: " + curse),
+                                                        false
+                                                );
+                                                return 0;
+                                            }
+
+                                            CurseManager.add(curse);
+
+                                            ctx.getSource().sendFeedback(
+                                                    () -> Text.literal("Curse activated: " + curse),
+                                                    false
+                                            );
+
+                                            return 1;
+                                        })
+                                )
+                        )
+
+                        // /curse remove
+                        .then(CommandManager.literal("remove")
+
+                                // /curse remove => list active curses
+                                .executes(ctx -> {
+                                    ctx.getSource().sendFeedback(
+                                            () -> Text.literal("Active curses: " + CurseManager.activeCurses),
+                                            false
+                                    );
+                                    return 1;
+                                })
+
+                                // /curse remove freeze
+                                .then(CommandManager.argument("curse", StringArgumentType.word())
+                                        .executes(ctx -> {
+
+                                            String curse = StringArgumentType.getString(ctx, "curse");
+
+                                            if (!CurseManager.activeCurses.contains(curse)) {
+                                                ctx.getSource().sendFeedback(
+                                                        () -> Text.literal("This curse is not active: " + curse),
+                                                        false
+                                                );
+                                                return 0;
+                                            }
+
+                                            CurseManager.remove(curse);
+
+                                            ctx.getSource().sendFeedback(
+                                                    () -> Text.literal("Curse removed: " + curse),
+                                                    false
+                                            );
+
+                                            return 1;
+                                        })
+                                )
+                        )
+
+                        // /curse timer <minutes>
+                        .then(CommandManager.literal("timer")
+                                .then(CommandManager.argument("minutes", IntegerArgumentType.integer(1, 1440))
+                                        .executes(ctx -> {
+
+                                            int minutes = IntegerArgumentType.getInteger(ctx, "minutes");
+
+                                            CurseManager.timerMinutes = minutes;
+
+                                            ctx.getSource().sendFeedback(
+                                                    () -> Text.literal("Curse timer set to " + minutes + " minutes"),
+                                                    false
+                                            );
+
+                                            return 1;
+                                        })
+                                )
+                        )
+
+                        // /curse toggle
+                        .then(CommandManager.literal("toggle")
+                                .executes(ctx -> {
+
+                                    CurseManager.autoEnabled = !CurseManager.autoEnabled;
+
+                                    ctx.getSource().sendFeedback(
+                                            () -> Text.literal(
+                                                    "Auto curses: " +
+                                                            (CurseManager.autoEnabled ? "§aENABLED" : "§cDISABLED")
+                                            ),
+                                            false
+                                    );
+
+                                    return 1;
+                                })
+                        )
+        );
+    }
+}
--- src/main/java/net/yadsoja/lifemod/curse/CurseManager.java
+++ src/main/java/net/yadsoja/lifemod/curse/CurseManager.java
@@ -1,153 +1,31 @@
-package net.yadsoja.lifemod.command;
+package net.yadsoja.lifemod.curse;
 
-import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.IntegerArgumentType;
-import com.mojang.brigadier.arguments.StringArgumentType;
+import java.util.HashSet;
+import java.util.Set;
 
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.text.Text;
+public class CurseManager {
 
-import net.yadsoja.lifemod.curse.CurseManager;
+    public static boolean autoEnabled = false;
+    public static int timerMinutes = 10;
 
-public class CurseCommand {
+    public static Set<String> activeCurses = new HashSet<>();
 
-    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+    public static Set<String> availableCurses = Set.of(
+            "freeze",
+            "blind",
+            "slow",
+            "hunger"
+    );
 
-        dispatcher.register(
-                CommandManager.literal("curse")
+    public static void add(String curse) {
+        activeCurses.add(curse);
+    }
 
-                        // /curse => info
-                        .executes(ctx -> {
-                            ctx.getSource().sendFeedback(
-                                    () -> Text.literal(
-                                            """
-                                            §6=== Curse System ===
-                                            /curse add
-                                            /curse add <name>
-                                            /curse remove
-                                            /curse remove <name>
-                                            /curse timer <minutes>
-                                            /curse toggle
-                                            """
-                                    ),
-                                    false
-                            );
-                            return 1;
-                        })
+    public static void remove(String curse) {
+        activeCurses.remove(curse);
+    }
 
-                        // /curse add
-                        .then(CommandManager.literal("add")
-
-                                // /curse add => list curses
-                                .executes(ctx -> {
-                                    ctx.getSource().sendFeedback(
-                                            () -> Text.literal("Available curses: " + CurseManager.availableCurses),
-                                            false
-                                    );
-                                    return 1;
-                                })
-
-                                // /curse add freeze
-                                .then(CommandManager.argument("curse", StringArgumentType.word())
-                                        .executes(ctx -> {
-
-                                            String curse = StringArgumentType.getString(ctx, "curse");
-
-                                            if (!CurseManager.availableCurses.contains(curse)) {
-                                                ctx.getSource().sendFeedback(
-                                                        () -> Text.literal("Unknown curse: " + curse),
-                                                        false
-                                                );
-                                                return 0;
-                                            }
-
-                                            CurseManager.add(curse);
-
-                                            ctx.getSource().sendFeedback(
-                                                    () -> Text.literal("Curse activated: " + curse),
-                                                    false
-                                            );
-
-                                            return 1;
-                                        })
-                                )
-                        )
-
-                        // /curse remove
-                        .then(CommandManager.literal("remove")
-
-                                // /curse remove => list active curses
-                                .executes(ctx -> {
-                                    ctx.getSource().sendFeedback(
-                                            () -> Text.literal("Active curses: " + CurseManager.activeCurses),
-                                            false
-                                    );
-                                    return 1;
-                                })
-
-                                // /curse remove freeze
-                                .then(CommandManager.argument("curse", StringArgumentType.word())
-                                        .executes(ctx -> {
-
-                                            String curse = StringArgumentType.getString(ctx, "curse");
-
-                                            if (!CurseManager.activeCurses.contains(curse)) {
-                                                ctx.getSource().sendFeedback(
-                                                        () -> Text.literal("This curse is not active: " + curse),
-                                                        false
-                                                );
-                                                return 0;
-                                            }
-
-                                            CurseManager.remove(curse);
-
-                                            ctx.getSource().sendFeedback(
-                                                    () -> Text.literal("Curse removed: " + curse),
-                                                    false
-                                            );
-
-                                            return 1;
-                                        })
-                                )
-                        )
-
-                        // /curse timer <minutes>
-                        .then(CommandManager.literal("timer")
-                                .then(CommandManager.argument("minutes", IntegerArgumentType.integer(1, 1440))
-                                        .executes(ctx -> {
-
-                                            int minutes = IntegerArgumentType.getInteger(ctx, "minutes");
-
-                                            CurseManager.timerMinutes = minutes;
-
-                                            ctx.getSource().sendFeedback(
-                                                    () -> Text.literal("Curse timer set to " + minutes + " minutes"),
-                                                    false
-                                            );
-
-                                            return 1;
-                                        })
-                                )
-                        )
-
-                        // /curse toggle
-                        .then(CommandManager.literal("toggle")
-                                .executes(ctx -> {
-
-                                    CurseManager.autoEnabled = !CurseManager.autoEnabled;
-
-                                    ctx.getSource().sendFeedback(
-                                            () -> Text.literal(
-                                                    "Auto curses: " +
-                                                            (CurseManager.autoEnabled ? "§aENABLED" : "§cDISABLED")
-                                            ),
-                                            false
-                                    );
-
-                                    return 1;
-                                })
-                        )
-        );
+    public static boolean has(String curse) {
+        return activeCurses.contains(curse);
     }
 }
